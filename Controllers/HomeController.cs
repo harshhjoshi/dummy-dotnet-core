15be4333d427a8672d3647e0c37684f68bde075b
Simone Bembi <wufe92@gmail.com>
2017-03-07 13:26:14 +0100

    Added request for multiple times

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,12 @@ namespace Dummy.Controllers
 {
     public class HomeController : Controller
     {
+
+        internal class Orario{
+            public DateTime Quando { get; set; }
+            public string Stato { get; set; }
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,12 +25,31 @@ namespace Dummy.Controllers
         {
             string nextStatus = "alla fine";
             DateTime mo = DateTime.Now;
-            DateTime leDiciotto = DateTime.Parse("18:00");
-            TimeSpan quantoManca = leDiciotto.Subtract(mo);
-            if( mo.Hour > 18 ){
+            TimeSpan quantoManca;
+            if( mo.Hour >= 18 && mo.Hour < 9 ){
                 DateTime leNove = DateTime.Parse("09:00").AddDays(1);
                 quantoManca = leNove.Subtract(mo);
                 nextStatus = "all'inizio";
+            }else if( mo.Hour >= 9 && mo.Hour < 11 ){
+                DateTime leUndici = DateTime.Parse("11:00");
+                quantoManca = leUndici.Subtract(mo);
+                nextStatus = "alla pausa del mattino";
+            }else if( mo.Hour >= 11 && mo.Hour < 13 ){
+                DateTime leTredici = DateTime.Parse("13:00");
+                quantoManca = leTredici.Subtract(mo);
+                nextStatus = "alla pausa pranzo";
+            }else if(mo.Hour >= 13 && mo.Hour < 14 ){
+                DateTime leQuattordici = DateTime.Parse("14:00");
+                quantoManca = leQuattordici.Subtract(mo);
+                nextStatus = "alla fine della pausa pranzo";
+            }else if(mo.Hour >= 14 && mo.Hour < 16 ){
+                DateTime leSedici = DateTime.Parse("16:00");
+                quantoManca = leSedici.Subtract(mo);
+                nextStatus = "alla pausa pomeridiana";
+            }else if(mo.Hour >= 16 && mo.Hour < 18 ){
+                DateTime leDiciotto = DateTime.Parse("18:00");
+                quantoManca = leDiciotto.Subtract(mo);
+                nextStatus = "alla fine della giornata";
             }
             int hours = quantoManca.Hours;
             int minutes = quantoManca.Minutes;
@@ -38,6 +63,62 @@ namespace Dummy.Controllers
                 seconds, secondsDescription,
                 nextStatus );
             return Json( new { result = risultato });
+        }
+
+        [ActionName("times")]
+        public IActionResult Times()
+        {
+            string nextStatus = "alla fine";
+            DateTime mo = DateTime.Now;
+            Orario[] orari = new Orario[]{
+                new Orario(){
+                    Quando = DateTime.Parse("09:00"),
+                    Stato = "all'inizio della giornata"
+                },
+                new Orario(){
+                    Quando = DateTime.Parse("11:00"),
+                    Stato = "alla pausa del mattino"
+                },
+                new Orario(){
+                    Quando = DateTime.Parse("13:00"),
+                    Stato = "alla pausa pranzo"
+                },
+                new Orario(){
+                    Quando = DateTime.Parse("14:00"),
+                    Stato = "alla fine della pausa pranzo"
+                },
+                new Orario(){
+                    Quando = DateTime.Parse("16:00"),
+                    Stato = "alla pausa pomeridiana"
+                },
+                new Orario(){
+                    Quando = DateTime.Parse("18:00"),
+                    Stato = "alla fine della giornata"
+                }
+            };
+            List<string> risultati = new List<string>();
+            foreach( Orario orario in orari ){
+                string risultatoParziale = "";
+                if( mo.CompareTo(orario.Quando ) > 0 ){
+                    risultatoParziale = String.Format("Mancano 0 secondi {0}!", orario.Stato);
+                }else{
+                    TimeSpan quantoManca = orario.Quando.Subtract(mo);
+                    int hours = quantoManca.Hours;
+                    int minutes = quantoManca.Minutes;
+                    int seconds = quantoManca.Seconds;
+                    string hoursDescription = hours == 1 ? "ora" : "ore";
+                    string minutesDescription = minutes == 1 ? "minuto" : "minuti";
+                    string secondsDescription = seconds == 1 ? "secondo" : "secondi";
+                    risultatoParziale = String.Format( "Mancano {0} {1}, {2} {3}, {4} {5} {6}!",
+                        hours, hoursDescription,
+                        minutes, minutesDescription,
+                        seconds, secondsDescription,
+                        nextStatus );
+                }
+                risultati.Add(risultatoParziale);
+            }
+            
+            return Json( new { results = risultati });
         }
 
         [HttpGet]
